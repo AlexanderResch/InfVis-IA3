b7950fce1be89074f4854d5c7bb135096da072cd
AlexanderResch <aresch3004@gmail.com>
2025-12-03 18:30:03 +0100

    Initial commit

---
+++ data/create_subset.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+input_file = "openpowerlifting-dataset.csv"
+
+output_file = "openpowerlifting_subset.csv"
+
+print("Loading CSV...")
+
+df = pd.read_csv(input_file, usecols=[
+    "Sex",
+    "Tested",
+    "Country",
+    "MeetCountry",
+    "TotalKg",
+    "Dots",
+    "Date"
+])
+
+print("Rows loaded:", len(df))
+print("Saving subset...")
+
+
+df.to_csv(output_file, index=False)
+
+print("Saved as:", output_file)
